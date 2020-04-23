4375a2ca07b914bdf5268188cf472b30db89eff5
chabode <rizqa.aulia@gmail.com>
2020-04-24 02:23:39 +0700

    Chart.js Jalan bos

--- controllers/chartController.js
+++ controllers/chartController.js
@@ -4,17 +4,17 @@ const sequelize = require('sequelize')
 class ChartController {
     static showAll(req,res){
         SelebShop.findAll({
-            group : ['SelebId'],
-            attributes: ['SelebId', [sequelize.fn('COUNT', 'SelebId'), 'Count']]
+            group : ['ShopId'],
+            attributes: ['ShopId', [sequelize.fn('COUNT', 'ShopId'), 'Count']]
         })
         .then(data=> {
             let label = []
             let dataNum = []
             for (let i = 0; i<data.length; i++){
-                label.push(data[i].dataValues.SelebId)
+                label.push(data[i].dataValues.ShopId)
                 dataNum.push(parseInt(data[i].dataValues.Count))
             }
-            console.log(dataNum)
+            // console.log(dataNum)
             res.render('chart',{label,dataNum})
             // res.send(data)
         })
