5bf7644b6ab4e82669c8a56f4ee85968702dab4e
chabode <rizqa.aulia@gmail.com>
2020-04-24 02:33:36 +0700

    Finish bosqu

--- controllers/chartController.js
+++ controllers/chartController.js
@@ -2,7 +2,10 @@ const {SelebShop, Seleb, Shop} = require('../models')
 const sequelize = require('sequelize')
 
 class ChartController {
-    static showAll(req,res){
+    static show(req,res){
+        res.render('pickChart')
+    }
+    static showAllShop(req,res){
         SelebShop.findAll({
             group : ['ShopId'],
             attributes: ['ShopId', [sequelize.fn('COUNT', 'ShopId'), 'Count']]
@@ -12,6 +15,27 @@ class ChartController {
             let dataNum = []
             for (let i = 0; i<data.length; i++){
                 label.push(data[i].dataValues.ShopId)
+                dataNum.push(parseInt(data[i].dataValues.Count))
+            }
+            // console.log(dataNum)
+            res.render('chart',{label,dataNum})
+            // res.send(data)
+        })
+        .catch(error=> {
+            res.send(error)
+        })
+    }
+
+    static showAllSeleb(req,res){
+        SelebShop.findAll({
+            group : ['SelebId'],
+            attributes: ['SelebId', [sequelize.fn('COUNT', 'SelebId'), 'Count']]
+        })
+        .then(data=> {
+            let label = []
+            let dataNum = []
+            for (let i = 0; i<data.length; i++){
+                label.push(data[i].dataValues.SelebId)
                 dataNum.push(parseInt(data[i].dataValues.Count))
             }
             // console.log(dataNum)
--- routers/chart.js
+++ routers/chart.js
@@ -2,18 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const ChartController = require('../controllers/chartController')
-function checkSession(req,res,next){
-    if(req.session.userId) next()
-    else res.send(`Not Authorized`)
-}
 
-router.get('/', ChartController.showAll)
-// router.get('/add', checkSession, SelebController.addForm)
-// router.post('/add', SelebController.add)
-// router.get('/:id/edit', checkSession, SelebController.editForm)
-// router.post('/:id/edit', SelebController.edit)
-// router.get('/:id/delete', checkSession, SelebController.delete)
-// router.get('/:seleb_id/collabs', SelebController.collabs)
+router.get('/', ChartController.show)
+router.get('/Seleb', ChartController.showAllSeleb)
+router.get('/Shop', ChartController.showAllShop)
 
 
 module.exports = router
